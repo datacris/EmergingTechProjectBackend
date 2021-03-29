0fb0e48085ea0011c0645504ebe68fb4c94939e9
datacris <datacristian1523@gmail.com>
2021-03-29 12:36:22 -0500

    Atlas DB implemented

--- config/env/development.js
+++ config/env/development.js
@@ -1,8 +1,8 @@
 ﻿//Development configuration options
 //To sign the session identifier, use a secret string
 module.exports = {
-    db: 'mongodb://localhost/emergingProject',
-    // db: 'mongodb+srv://datacris:ohbPLaLIpAX1sNPB@cluster0.a7dl2.mongodb.net/emergingTechProjectDb?retryWrites=true&w=majority',
+    // db: 'mongodb://localhost/emergingProject',
+    db: 'mongodb+srv://datacris:lukwG5j6umKb9HnM@cluster0.a7dl2.mongodb.net/emergingTechProjectDb?retryWrites=true&w=majority',
     sessionSecret: 'developmentSessionSecret',
     secretKey: 'real_secret'
     
