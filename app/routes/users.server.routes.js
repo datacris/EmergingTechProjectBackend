e09accdcdf79cb0f7f87c5e2028b87120e7b89a5
datacris <datacristian1523@gmail.com>
2021-04-05 15:26:54 -0500

    No significant changes

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -19,29 +19,5 @@ module.exports = function (app) {
         .get(users.userById);
 
 
-    // app.route('/createStudent')
-    //     .post(students.create);
-
-    // app.route('/students')
-    //     .get(students.list);
-
-    // // Set up the 'courses' parameterized routes 
-    // app.route('/students/:studentId')
-    //     .get(students.studentByID);
-
-    // app.get('/read_cookie', students.isSignedIn);
-    // app.post('/signin', students.authenticate);
-
-    // app.route('/enrollCourse')
-    //     .put(students.requiresLogin, students.enrollCourse)
-
-    // app.route('/dropCourse')
-    //     .put(students.requiresLogin, students.dropCourse)
-
-    // app.route('/coursesByStudent/:studentId')
-    //     .get(students.coursesByStudent);
-
-    // app.get('/signout', students.signout);
-
 };
 
--- server.js
+++ server.js
@@ -5,15 +5,34 @@
 //value if itdoesn 't exist.
 // Set the 'NODE_ENV' variable
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 // Load the module dependencies
 var mongoose = require('./config/mongoose'),
     express = require('./config/express');
+
+//Swagger for backend API documentation
+    const swaggerJsDoc = require('swagger-jsdoc');
+    const swaggerUi = require('swagger-ui-express');
+
 // Create a new Mongoose connection instance
 var db = mongoose();
 // Create a new Express application instance
 var app = express();
 // Use the Express application instance to listen to the '3000' port
 const port = process.env.PORT || 5000
+
+const swaggerOptions = {
+    swaggerDefinition:{
+        info: {
+            title: 'App title',
+            version: '1.0.0'
+        }
+    },
+    apis: ['./app/controllers/users.server.controller.js'],
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.listen(port);
 // Use the module.exports property to expose our Express application instance for external usage
 module.exports = app; //returns the application object
